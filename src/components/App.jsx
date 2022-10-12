3ef007a191ba739d3316f6570078c27c1ebc5e3c
Andrii <78295684+syniuk-a@users.noreply.github.com>
2022-10-12 08:06:11 +0300

    commit

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,8 @@
+import { Recipe } from "./recipe";
+import recipes from "./"
+
 export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
+  return <div>
+    <Recipe recipe={ } />
+  </div>;
 };
